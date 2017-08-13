fbf7ad2ba7717b80b6ad67be4ae6e931dcd7bfa0
Seema Narasimha Swamy <seema.n.swamy@campus.tu-berlin.de>
2017-08-13 16:47:50 +0200

    regression modified

--- flinkml/src/main/scala/de/tuberlin/dima/bdapro/flinkml/MLRegression.scala
+++ flinkml/src/main/scala/de/tuberlin/dima/bdapro/flinkml/MLRegression.scala
@@ -1,6 +1,8 @@
 package de.tuberlin.dima.bdapro.flinkml
 
-import org.apache.flink.api.scala.DataSet
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.api.scala._
+import org.apache.flink.ml.RichExecutionEnvironment
 import org.apache.flink.ml.MLUtils
 import org.apache.flink.ml.common.LabeledVector
 import org.apache.flink.ml.preprocessing.Splitter
@@ -11,6 +13,16 @@ object MLRegression {
   def main(args:Array[String]) ={
     
     print("Hello World")
+
+    val params: ParameterTool = ParameterTool.fromArgs(args)
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    env.getConfig.setGlobalJobParameters(params)
+
+    val pathToTrainingFile = params.getRequired("training")
+    val pathToTestingFile = params.get("testing")
+
+    // Read the training data set, from a LibSVM formatted file
+    val trainingDS: DataSet[LabeledVector] = env.readLibSVM(pathToTrainingFile)
       
     val dataSet: DataSet[LabeledVector] = MLUtils.readLibSVM(env, "/path/to/svmguide1")
     val trainTestData: DataSet[TrainTestDataSet] = Splitter.trainTestSplit(dataSet)
